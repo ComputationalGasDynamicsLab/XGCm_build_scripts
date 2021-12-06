d2e81ea1aca0638e118dd8928233dc4f1f7736fc
Chonglin Zhang <zhangchonglin@gmail.com>
2021-12-06 13:18:04 -0500

    add amgx build script

---
+++ AmgX/README.md
@@ -0,0 +1,4 @@
+build scripts for AmgX on Summit
+
+xgcm build script with AmgX is not working properly, failing at the final link step;
+currently the final link step is being done manually
---
+++ AmgX/amgx_build_sm70_opt_rhel8.sh
@@ -0,0 +1,25 @@
+module load gcc/7.5.0
+module load cuda/10.2.89
+module load cmake/3.20.2
+
+export cuda=$CUDA_DIR
+export PATH=$cuda/bin:$PATH
+export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
+export installroot=$PWD
+export srcroot=$installroot/../                                                                                              
+
+# AmgX
+export amgxsrc=$srcroot/amgx
+export amgx=$installroot/amgx/install
+
+cd $installroot
+mkdir -p amgx/build
+cd amgx/build
+
+cmake $amgxsrc -DCMAKE_BUILD_TYPE=Release \
+               -DBUILD_SHARED_LIBS=ON -DCUDA_ARCH=70 \
+               -DCMAKE_CXX_FLAGS="-mno-float128"
+               -DCMAKE_CUDA_FLAGS="-Xcompiler -mno-float128"
+               -DCMAKE_INSTALL_PREFIX=$amgx
+
+make -j8 install

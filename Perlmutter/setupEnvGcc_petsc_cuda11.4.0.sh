361e4683d5629082a6dc80f355912d95f9c466c9
Chonglin Zhang <zhangchonglin@gmail.com>
2021-12-13 06:39:43 -0800

    update PETSc script with cudatoolkit/21.9_11.4 (cuda 11.4)

---
+++ Perlmutter/setupEnvGcc_petsc_cuda11.4.0.sh
@@ -0,0 +1,18 @@
+module load PrgEnv-gnu
+module load cudatoolkit/21.9_11.4
+module load cpe-cuda
+module load cray-pmi/6.0.16
+module load cmake/3.22.0
+module load valgrind4hpc/2.12.4
+
+export cuda=$CRAY_CUDATOOLKIT_DIR
+export PATH=$cuda/bin:$PATH
+export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
+export installroot=$PWD
+export PETSC_DIR=$installroot
+export PETSC_ARCH=arch-perlmutter
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PETSC_DIR/$PETSC_ARCH/lib/pkgconfig
+
+export OMPI_CXX=CC
+export OMPI_CC=cc
+export OMPI_FC=ftn

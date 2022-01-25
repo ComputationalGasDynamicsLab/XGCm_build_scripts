01d8873a03997493c07e121034b52ca90d7d6884
Chonglin Zhang <zhangchonglin@gmail.com>
2022-01-25 11:09:56 -0800

    update kokkos-tool build script and run script

--- Perlmutter/kokkos_tools_build.sh
+++ Perlmutter/kokkos_tools_build.sh
@@ -1,4 +1,4 @@
-export CUDA_ROOT=$CRAY_CUDATOOLKIT_DIR
+export CUDA_ROOT=$CUDA_HOME
 if [ "$#" -lt 1 ]; then
   ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 else
@@ -17,6 +17,13 @@ make -f $ROOT_DIR/profiling/simple-kernel-timer-json/Makefile
 make -f $ROOT_DIR/profiling/simple-kernel-timer/Makefile
 make -f $ROOT_DIR/profiling/space-time-stack/Makefile
 make -f $ROOT_DIR/profiling/systemtap-connector/Makefile
-make -f $ROOT_DIR/profiling/timemory-connector/Makefile
-make -f $ROOT_DIR/profiling/vtune-connector/Makefile
-make -f $ROOT_DIR/profiling/vtune-focused-connector/Makefile
+if [ -z "${VTUNE_HOME}" ]; then
+  echo ""
+  echo "========================================="
+  echo "Set VTUNE_HOME to build vtune connectors."
+  echo "========================================="
+  echo ""
+else
+  make -f $ROOT_DIR/profiling/vtune-connector/Makefile
+  make -f $ROOT_DIR/profiling/vtune-focused-connector/Makefile
+fi
--- Perlmutter/xgcm_submission_perlmutter.sh
+++ Perlmutter/xgcm_submission_perlmutter.sh
@@ -11,12 +11,13 @@
 #SBATCH --job-name=Cyclone_590k_nphi=8
 
 module load PrgEnv-gnu
+module load cudatoolkit/21.9_11.4
 module load cpe-cuda
-module load cmake/3.20.5
-module load cuda/11.1.1
-
+module load craype-accel-nvidia80
 export SLURM_CPU_BIND="cores"
+export MPICH_ABORT_ON_ERROR=1
+ulimit -c unlimited
 
 srun ./XGCm --kokkos-threads=1 590kmesh.osh 590kmesh_6.cpn \
-1 1 bfs bfs 0 1 0 3 input_20million_nrho=3 petsc petsc_xgcm.rc \
+1 1 bfs bfs 1 1 0 3 input_20million_nrho=3 petsc petsc_xgcm.rc \
 -use_gpu_aware_mpi 0

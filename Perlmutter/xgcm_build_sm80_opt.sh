a021c4fa41288221e86255d3089f9ed31ad498e2
Chonglin Zhang <zhangchonglin@gmail.com>
2022-01-26 13:32:02 -0800

    update cmake option to run Cyclone test case

--- Perlmutter/xgcm_build_sm80_opt.sh
+++ Perlmutter/xgcm_build_sm80_opt.sh
@@ -48,7 +48,7 @@ cd xgcm_opt/build
 rm CMakeCache.txt
 cmake $xgcmsrc -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=CC \
                -DIS_TESTING=ON -DCMAKE_INSTALL_PREFIX=$xgcm \
-               -DXGCM_GPU_SOLVE=ON -DXGCM_INIT_GENE_PERT=OFF \
+               -DXGCM_GPU_SOLVE=ON -DXGCM_INIT_GENE_PERT=ON \
                -DXGC_DATA_DIR=$xgcmtestdir \
                -DXGCM_SNES_SOLVE=OFF \
                -DCMAKE_CXX_FLAGS="-g"

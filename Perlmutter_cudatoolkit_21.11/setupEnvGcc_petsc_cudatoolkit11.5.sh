baffb25e28511cf338670b9f5b509f8ac5f653e1
Chonglin Zhang <zhangchonglin@gmail.com>
2022-03-07 23:55:55 -0800

    add build script use cudatoolkit_21.11_11.5

---
+++ Perlmutter_cudatoolkit_21.11/setupEnvGcc_petsc_cudatoolkit11.5.sh
@@ -0,0 +1,22 @@
+module load PrgEnv-gnu
+module load cudatoolkit/21.11_11.5
+module load cpe-cuda
+module unload craype-network-ofi
+module load craype-network-ucx
+module unload cray-mpich
+module load cray-mpich-ucx/8.1.13
+module load cmake/3.22.0
+module load valgrind4hpc/2.12.4
+module unload darshan
+
+export cuda=$CRAY_CUDATOOLKIT_DIR
+export PATH=$cuda/bin:$PATH
+export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
+export installroot=$PWD
+export PETSC_DIR=$installroot
+export PETSC_ARCH=arch-perlmutter
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PETSC_DIR/$PETSC_ARCH/lib/pkgconfig
+
+export OMPI_CXX=CC
+export OMPI_CC=cc
+export OMPI_FC=ftn

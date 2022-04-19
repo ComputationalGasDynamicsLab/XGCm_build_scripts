9bed44f2001f78bf1aa6b323fc07c44406088ef2
Chonglin Zhang <zhangchonglin@gmail.com>
2022-04-19 16:24:57 -0700

    add nvhpc build scripts

---
+++ Perlmutter_nvhpc/arch-perlmutter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+if __name__ == '__main__':
+  import os
+  import sys
+  sys.path.insert(0, os.path.abspath('config'))
+  import configure
+  configure_options = [
+    '--with-cc=cc',
+    '--with-cxx=CC',
+    '--with-fc=ftn',
+    '--with-shared-libraries=1',
+    '--with-debugging=no',
+    '--COPTFLAGS=-g -fast -fPIC',
+    '--CXXOPTFLAGS=-g -fast -fPIC',
+    '--FOPTFLAGS=-g -fast -fPIC',
+    '--with-cuda',
+    '--with-cuda-gencodearch=80',
+    '--with-cudac=nvcc',
+    '--download-metis',
+    '--download-parmetis',
+    '--download-openblas'
+    '--with-make-np=8',
+  ]
+  configure.petsc_configure(configure_options)
---
+++ Perlmutter_nvhpc/pumipic_build_sm80_opt.sh
@@ -0,0 +1,45 @@
+module load cudatoolkit/11.5
+module load craype-accel-nvidia80
+module unload craype-network-ofi
+module load craype-network-ucx
+module unload cray-mpich
+module load cray-mpich-ucx/8.1.13
+module load cmake/3.22.0
+
+export cuda=$CRAY_CUDATOOLKIT_DIR
+export PATH=$cuda/bin:$PATH
+export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
+export installroot=$PWD
+export srcroot=$installroot/../                                                                                              
+
+# kokkos
+export kk=$installroot/kokkos/install
+export kksrc=$srcroot/kokkos
+
+# omega_h
+export oh=$installroot/omega_h/install
+# particle structure
+export ps=$installroot/particle_structures/install
+
+#EnGPar
+export EnGPar=$installroot/EnGPar/install
+
+# pumi-pic
+export pumipicsrc=$srcroot/pumi-pic
+export testdir=$pumipicsrc/pumipic-data
+export pumipic=$installroot/pumi-pic/install
+
+export CMAKE_PREFIX_PATH=$kk:$oh:$EnGPar:$CMAKE_PREFIX_PATH
+export OMPI_CXX=$kksrc/bin/nvcc_wrapper
+
+cd $installroot
+mkdir -p pumi-pic/build
+cd pumi-pic/build
+cmake $pumipicsrc -DCMAKE_BUILD_TYPE=Release \
+                  -DCMAKE_CXX_COMPILER=CC \
+                  -DIS_TESTING=OFF \
+                  -DCMAKE_INSTALL_PREFIX=$pumipic \
+                  -DTEST_DATA_DIR=$testdir
+
+
+make -j8 install

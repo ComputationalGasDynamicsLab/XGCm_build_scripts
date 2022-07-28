6f457b3f26da13c6f14554dc3fed0a9f2f04238a
Chonglin Zhang <zhangchonglin@gmail.com>
2022-07-28 11:50:59 -0700

    delete old build scripts

--- Perlmutter_cudatoolkit_21.11/xgcm_modules.sh
+++
@@ -1,9 +0,0 @@
-module load PrgEnv-gnu
-module load cudatoolkit/21.9_11.4
-module load cpe-cuda
-module load craype-accel-nvidia80
-module unload craype-network-ofi
-module load craype-network-ucx
-module unload cray-mpich
-module load cray-mpich-ucx/8.1.12
-module load cmake/3.22.0

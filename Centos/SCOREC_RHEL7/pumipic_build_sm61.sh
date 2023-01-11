ef8bd93984f0b5c4bf73133b6e9a4c8456cb83ad
Chonglin Zhang <zhangchonglin@gmail.com>
2023-01-11 14:16:34 -0500

    update CentOS 7 build script for pumi-pic

--- Centos/SCOREC_RHEL7/pumipic_build_sm61.sh
+++ Centos/SCOREC_RHEL7/pumipic_build_sm61.sh
@@ -21,22 +21,21 @@ export EnGPar=$installroot/EnGPar/install
 
 # pumi-pic
 export pumipicsrc=$srcroot/pumi-pic
-export testdir=$pumipicsrc/pumipic-data
 export pumipic=$installroot/pumi-pic/install
 
 export CMAKE_PREFIX_PATH=$kk:$oh:$EnGPar:$CMAKE_PREFIX_PATH
-export MPICH_CXX=$kksrc/bin/nvcc_wrapper
 
 cd $installroot
 mkdir -p pumi-pic/build
 cd pumi-pic/build
 
-cmake $pumipicsrc -DCMAKE_CXX_COMPILER=mpicxx \
-                  -DIS_TESTING=ON \
+cmake $pumipicsrc -DCMAKE_BUILD_TYPE=Release \
                   -DBUILD_SHARED_LIBS=OFF \
+                  -DCMAKE_CXX_COMPILER=mpicxx \
+                  -DCMAKE_CXX_FLAGS="-fPIC" \
+                  -DENABLE_CABANA=OFF \
                   -DPS_IS_TESTING=ON \
                   -DCMAKE_INSTALL_PREFIX=$pumipic \
-                  -DTEST_DATA_DIR=$testdir
 
 make -j4 install
 ctest

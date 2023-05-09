2f70bdd440a1d6a68a4dfee3b052687970c79547
Chonglin Zhang <zhangchonglin@gmail.com>
2023-05-09 08:21:37 -0700

    update build scripts

--- Perlmutter/Perlmutter_gcc11.2.0_cuda11.7_kokkos3.7.01/xgcm_build_sm80_petsc_3.16.6.sh
+++ Perlmutter/Perlmutter_gcc11.2.0_cuda11.7_kokkos3.7.01/xgcm_build_sm80_petsc_3.16.6.sh
@@ -35,7 +35,7 @@ export xgcmsrc=$srcroot/xgcm
 export xgcm=$installroot/xgcm_opt/install
 export xgcmtestdir=$xgcmsrc/xgc1_data
 
-export PETSC_DIR=$installroot/../petsc
+export PETSC_DIR=$installroot/../petsc_3.16.6
 export PETSC_ARCH=arch-perlmutter
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PETSC_DIR/$PETSC_ARCH/lib/pkgconfig
 export CMAKE_PREFIX_PATH=$kk:$oh:$EnGPar:$cabana:$pumipic:$CMAKE_PREFIX_PATH
@@ -43,13 +43,13 @@ export OMPI_CXX=$kksrc/bin/nvcc_wrapper
 
 
 cd $installroot
-mkdir -p xgcm_opt/build
-cd xgcm_opt/build
+mkdir -p xgcm_petsc_3.16.6/build
+cd xgcm_petsc_3.16.6/build
 
 # below is just a simple way to handle a strange issue, when rebuilding,
 # g++: error: unrecognized command-line option '-rpath'
 rm CMakeCache.txt
-cmake $xgcmsrc -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=CC \
+cmake $xgcmsrc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_COMPILER=CC \
                -DIS_TESTING=ON -DCMAKE_INSTALL_PREFIX=$xgcm \
                -DXGCM_GPU_SOLVE=ON -DXGCM_INIT_GENE_PERT=ON \
                -DXGC_DATA_DIR=$xgcmtestdir \

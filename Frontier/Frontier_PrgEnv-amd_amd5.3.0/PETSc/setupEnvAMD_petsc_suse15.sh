e555529878dce1f444964b698b83d3482ae7a2dc
Chonglin Zhang <zhangchonglin@gmail.com>
2023-06-28 13:54:31 -0400

    add PETSc build scripts

---
+++ Frontier/Frontier_PrgEnv-amd_amd5.3.0/PETSc/setupEnvAMD_petsc_suse15.sh
@@ -0,0 +1,17 @@
+module load PrgEnv-amd
+module load cmake/3.23.2
+module load craype-accel-amd-gfx90a
+module load amd/5.3.0
+module load valgrind4hpc/2.12.11
+module load hdf5/1.14.0
+export CRAYPE_LINK_TYPE=dynamic
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+export installroot=$PWD
+export PETSC_DIR=$installroot
+export PETSC_ARCH=arch-frontier
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PETSC_DIR/$PETSC_ARCH/lib/pkgconfig
+
+export OMPI_CXX=CC
+export OMPI_CC=cc
+export OMPI_FC=ftn
